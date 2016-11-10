ce3cb7c87c4a458bcdb35492a895015b60d76196
Nikita Shishin <nktshn06@gmail.com>
2016-11-11 01:50:58 +0600

    Trash from format method has been removed

--- src/main/java/pack/Formatter.java
+++ src/main/java/pack/Formatter.java
@@ -21,35 +21,21 @@ package pack;
      * @param sb  - string
      *
      */
-
        public void format(char[] buff, final StringBuilder sb) throws FormatterException { //method receiving
           final int five = 5;
-
                try {
-                   int count = 0;  //simple counter of ';'
                    int tabs = 0;   //counter of current paragraphs
-
-                   for (int i = 0; i < sb.length(); i++) {  //counting ';' symbols
-                       if (buff[i] == ';') {
-                           count++;
-                       }
-                   }
-                   //MAIN CYCLE:
-                   for (int i = 0, j = 0; i < buff.length; i++) {
-                       if (j > count) {   //second limiter of cycle
-                           break;
-                       } else {
+                                //MAIN CYCLE:
+                   for (int i = 0; i < buff.length; i++) {
                            if (buff[i] == ';') {
                                sb.insert(i + 1, '\n');
                                for (int k = 0; k < tabs; k++) {
                                    sb.insert(i + 2, "    "); //inserting paragraphs
-                                   // to each new lines
+                                                            // to each new lines
                                }
                                String s = String.valueOf(sb); //overwriting our
-                               // char array
-                               buff = s.toCharArray();        //
-                               j++;
-                               count++;
+                                                            // char array
+                               buff = s.toCharArray();
                            }
                            if (buff[i] == '{') {
                                if (buff[i - 1] == ']') {      //protection of
@@ -63,8 +49,6 @@ package pack;
                                }
                                String s = String.valueOf(sb);
                                buff = s.toCharArray();
-                               j += five;
-                               count += five;
                            }
                            if (buff[i] == '}') {
                                if (buff[i + 1] == ';') { //protection of initialized
@@ -78,12 +62,8 @@ package pack;
                                }
                                String s = String.valueOf(sb);
                                buff = s.toCharArray();
-                               j++;
-                               count++;
                            }
                        }
-
-                   }
                } catch (Exception e) {
                    throw new FormatterException("Formatting failed", e);
                }
